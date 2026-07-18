88a6c2dae6e60e3c91a092a6add4b23d7f537365
PRAJIN <pshingala519@rku.ac.in>
2026-07-18 16:45:52 +0530

    Build frontend assets

--- database/factories/VehicleRecordFactory.php
+++ database/factories/VehicleRecordFactory.php
@@ -16,27 +16,32 @@ class VehicleRecordFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        $issueDate = $this->faker->dateTimeBetween('-1 year', 'now');
-        $expiryDate = (clone $issueDate)->modify('+' . $this->faker->randomElement([6, 12]) . ' months');
-        
-        $states = ['GJ', 'MH', 'DL', 'KA', 'TN', 'UP'];
-        $vehicleNumber = $this->faker->randomElement($states) . $this->faker->numberBetween(10, 99) . $this->faker->lexify('??') . $this->faker->numberBetween(1000, 9999);
+{
+    $issueDate = fake()->dateTimeBetween('-1 year', 'now');
 
-        return [
-            'user_id' => 1, // Assume user 1 is the main center owner for the seeder
-            'customer_name' => $this->faker->name(),
-            'customer_mobile' => '9' . $this->faker->numerify('#########'),
-            'vehicle_number' => strtoupper($vehicleNumber),
-            'vehicle_type' => $this->faker->randomElement(['Bike', 'Car', 'Auto', 'Truck', 'Bus', 'Other']),
-            'fuel_type' => $this->faker->randomElement(['Petrol', 'Diesel', 'CNG', 'LPG', 'Electric', 'Hybrid']),
-            'puc_certificate_number' => strtoupper($this->faker->bothify('PUC-####-?????')),
-            'issue_date' => $issueDate->format('Y-m-d'),
-            'expiry_date' => $expiryDate->format('Y-m-d'),
-            'puc_price' => $this->faker->randomElement([100, 150, 200, 300]),
-            'notes' => null,
-            'created_at' => $issueDate->format('Y-m-d H:i:s'),
-            'updated_at' => clone $issueDate,
-        ];
-    }
+    $expiryDate = (clone $issueDate)->modify('+' . fake()->randomElement([6, 12]) . ' months');
+
+    $states = ['GJ', 'MH', 'DL', 'KA', 'TN', 'UP'];
+
+    $vehicleNumber = fake()->randomElement($states)
+        . fake()->numberBetween(10, 99)
+        . fake()->lexify('??')
+        . fake()->numberBetween(1000, 9999);
+
+    return [
+        'user_id' => 1,
+        'customer_name' => fake()->name(),
+        'customer_mobile' => '9' . fake()->numerify('#########'),
+        'vehicle_number' => strtoupper($vehicleNumber),
+        'vehicle_type' => fake()->randomElement(['Bike', 'Car', 'Auto', 'Truck', 'Bus', 'Other']),
+        'fuel_type' => fake()->randomElement(['Petrol', 'Diesel', 'CNG', 'LPG', 'Electric', 'Hybrid']),
+        'puc_certificate_number' => strtoupper(fake()->bothify('PUC-####-?????')),
+        'issue_date' => $issueDate->format('Y-m-d'),
+        'expiry_date' => $expiryDate->format('Y-m-d'),
+        'puc_price' => fake()->randomElement([100, 150, 200, 300]),
+        'notes' => null,
+        'created_at' => $issueDate,
+        'updated_at' => $issueDate,
+    ];
+}
 }
